46db70689e90f64fc5f5c2e7cce27f977e2b4f80
vkj8189 <53281673+vkj8189@users.noreply.github.com>
2019-08-18 17:56:52 +1200

    Myversion

---
+++ Marball/Assets/InventoryScrollList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public Sprite icon;
+}
+
+public class InventoryScrollList : MonoBehaviour
+{
+    public List<Item> itemList;
+    public Transform contentPanel;
+    public InventoryScrollList otherInventory;
+    public SimpleObjectPool buttonObjectPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshDisplay();
+    }
+
+    public void RefreshDisplay()
+    {
+        AddButton();
+    }
+    private void AddButton()
+    {
+        for(int i = 0; i < itemList.Count; i++)
+        {
+            Item item = itemList[i];
+            GameObject newButton = buttonObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            SampleButton sampleButton = newButton.GetComponent<SampleButton>();
+            sampleButton.Setup(item, this);
+        }
+    }
+}
---
+++ Marball/Assets/SampleButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour
+{
+    public Button button;
+    public Text nameLabel;
+    public Image iconImage;
+
+    private Item item;
+    private InventoryScrollList scrollList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Setup(Item currentItem, InventoryScrollList currentScrollList)
+    {
+        item = currentItem;
+        nameLabel.text = item.itemName;
+        iconImage.sprite = item.icon;
+
+        scrollList = currentScrollList;
+    }
+
+}

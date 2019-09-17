17b05510b5ae71b864465c6d693760b73c0b835c
RobertDumagan <hgr7255@autuni.ac.nz>
2019-09-17 13:01:45 +1200

    Create TouchRotate.cs

---
+++ Marball/Marball/TouchRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+    public float speed = 2;
+    private Vector2 touchDeltaPosition;
+
+    void Update()
+    {
+        if (Input.touchCount > 0 &&
+       Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+
+            // Get movement of the finger since last frame
+            touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across Z plane
+            transform.Rotate(0,
+                        0, touchDeltaPosition.x * speed - touchDeltaPosition.y * speed);
+        }
+    }
+}
+
